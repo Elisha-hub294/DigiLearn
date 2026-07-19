660eadeb5926973762a28af289aa618b5bc0963e
Elisha <elishabagalw@gmail.com>
2026-07-19 15:58:43 +0300

    feat: add Firebase configuration and initialization for Firestore

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKAYN1h2binUbKcJAZhcXZL8KQ_rBs21I",
+  authDomain: "digilearn-af86d.firebaseapp.com",
+  projectId: "digilearn-af86d",
+  storageBucket: "digilearn-af86d.firebasestorage.app",
+  messagingSenderId: "851245099108",
+  appId: "1:851245099108:web:fab0d172231b2a550a9771",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
